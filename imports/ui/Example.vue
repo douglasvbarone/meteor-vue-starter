c14d75d5be6eff6f4a353d691955f049c63a5cea
Douglas Barone <douglasvbarone@gmail.com>
2019-04-10 20:17:43 +0000

    Minor fixes

--- imports/ui/Example.vue
+++ imports/ui/Example.vue
@@ -1,22 +1,25 @@
 <template>
-    <v-card>
-        <v-card-title class="primary white--text headline">An example of Vue with Meteor reactivity</v-card-title>
-        <v-card-text>
-            <v-text-field label="Add some text to database:" v-model="text"/>
+    <div style="max-width: 640px; margin: auto">
+        <v-card class="mb-4">
+            <v-card-title class="primary white--text headline">An example of Vue with Meteor reactivity</v-card-title>
+            <v-card-text>
+                <v-text-field label="Add some text to the database" v-model="text"/>
+
+            </v-card-text>
+            <v-card-actions>
+<!--                <div v-if="!$subReady.Time">Loading...</div>-->
+                <v-spacer/>
+                <v-btn @click="onAddExample" color="primary">Add</v-btn>
+            </v-card-actions>
+
+        </v-card>
+        <div>
             <v-chip @click="onDelExample(example._id)" v-for="example in allExamples" :key="example._id">
                 {{example.text}}
             </v-chip>
-            <div>
-                Click to delete.
-            </div>
-        </v-card-text>
-        <v-card-actions>
-            <div v-if="!$subReady.Time">{{$subReady}}Loading...</div>
-            <v-spacer/>
-            <v-btn @click="onAddExample" color="primary">Add</v-btn>
-        </v-card-actions>
-
-    </v-card>
+        </div>
+        Click to delete.
+    </div>
 </template>
 
 <script>
@@ -28,10 +31,6 @@
             text: ''
         }),
         methods: {
-            $subscribe:{
-                'Example': []
-            },
-
             onAddExample() {
                 Example.insert({text: this.text})
                 this.text = ''
@@ -42,6 +41,10 @@
             }
         },
         meteor: {
+            $subscribe: {
+                'Example': []
+            },
+
             allExamples() {
                 return Example.find({}).fetch()
             }
